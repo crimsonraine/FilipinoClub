eab730d03ab5be3a6236dfa299ae927b817eba36
crimsonraine <wukimberley98@gmail.com>
2024-03-06 15:18:51 -0500

    big update

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
+// MODULES
 const express = require( "express" );
 const app = express();
 const port = 3000;
 
+const logger = require("morgan");
+
+app.use(logger("dev"));
+app.use(express.static(__dirname + '/public'));
+
+// ROUTERS
 app.get( "/", ( req, res ) => {
-    res.send( "<h1>Hello world!</h1>" );
+    console.log("GET /");
+    res.sendFile( __dirname + "/views/index.html" );
+} );
+
+app.get( "/about", ( req, res ) => {
+    res.sendFile( __dirname + "/views/about.html" );
+} );``
+
+app.get( "/board", ( req, res ) => {
+    res.sendFile( __dirname + "/views/board.html" );
+} );
+
+app.get( "/contact", ( req, res ) => {
+    res.sendFile( __dirname + "/views/contact.html" );
+} );
+
+app.get( "/gallery", ( req, res ) => {
+    res.sendFile( __dirname + "/views/gallery.html" );
 } );
 
 app.listen( port, () => {
